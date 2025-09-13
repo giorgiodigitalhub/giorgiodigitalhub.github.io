2e6057764a63f110b7dd3198cb67d658e5c69af9
giorgiodigitalhub <giaccainbelgio@gmail.com>
2025-09-13 16:01:51 +0200

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const footerHTML = `
+	
+	  <footer class="w3-container w3-center w3-padding-32 small">
+		<hr>
+		<p>© 2025 • Sito di Giorgio Giaccaglini - docente di Informatica · Tema minimalista con W3.CSS</p>
+		<p>Proponi una correzione: <a href="mailto:giaccaglini.giorgio@liceodavincijesi.edu.it">giaccaglini.giorgio@liceodavincijesi.edu.it</a></p>
+	  </footer>
+    `;
+    document.getElementById('footer-container').innerHTML = footerHTML;
+});
